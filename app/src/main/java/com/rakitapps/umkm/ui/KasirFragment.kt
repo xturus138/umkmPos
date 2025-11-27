62017bac9c1c2a697bfcb6b4fdcb2caa4f86a2c2
xturus138 <radityafauzan138@gmail.com>
2025-11-27 14:57:38 +0700

    Add KasirFragment layout and implementation, update colors, and create export button background

--- app/src/main/java/com/rakitapps/umkm/ui/KasirFragment.kt
+++ app/src/main/java/com/rakitapps/umkm/ui/KasirFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [DashboardFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DashboardFragment : Fragment() {
+class KasirFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return inflater.inflate(R.layout.fragment_kasir, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class DashboardFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DashboardFragment().apply {
+            KasirFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
